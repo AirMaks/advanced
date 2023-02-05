a960ba2f9a441e662c9132739d98e9378729d541
AirMaks <rm1010@yandex.ru>
2023-02-05 14:53:09 +0300

    fix gitignore

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,32 @@
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const {paths, mode, isDev} = options;
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true
+        },
+        optimization: {
+            minimizer: [
+                new CssMinimizerPlugin(),
+            ],
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(options)
+        },
+        resolve: buildResolvers(),
+        "devtool": isDev ? "inline-source-map" : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined
+    }
+}
