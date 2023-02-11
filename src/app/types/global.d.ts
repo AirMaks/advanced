dfb47f4b50eb71cea4891382ea73bae3ffa70f48
AirMaks <rm1010@yandex.ru>
2023-02-11 14:36:14 +0300

    added svg and file loaders

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,4 +1,13 @@
 declare module '*.scss' {
-    const css: { [key: string]: string };
-    export default css;
-  }
+  const css: { [key: string]: string };
+  export default css;
+}
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg' {
+    import React from 'react';
+    const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+    export default SVG;
+}
