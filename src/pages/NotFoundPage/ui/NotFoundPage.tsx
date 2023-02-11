b7d07177836f25fbe094ec8161838b2f12e51a2d
AirMaks <rm1010@yandex.ru>
2023-02-11 22:29:47 +0300

    add not found page

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import { cn } from "shared/lib/classNames/classNames";
+import cls from "./NotFoundPage.module.scss";
+import { useTranslation } from "react-i18next";
+
+interface NotFoundPageProps {
+    className?: string;
+}
+export const NotFoundPage = ({ className }: NotFoundPageProps) => {
+    const { t } = useTranslation("not-found");
+    return <div className={cn(cls.NotFoundPage, {}, [className])}>{t("Страница не найдена")}</div>;
+};
