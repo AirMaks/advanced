e62e738152ed4d6f578577a6fc333e184c9ae2c2
AirMaks <rm1010@yandex.ru>
2023-02-12 18:12:39 +0300

    add error boundary

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const MainPage = () => {
     const { t } = useTranslation("main");
+    const [error, setError] = useState(false);
+    const err = () => setError(true);
+
+    useEffect(() => {
+        if (error) throw new Error();
+    }, [error]);
+
     return (
         <div>
             {t("Привет")}
             {t("Главная страница")}
+            <button onClick={err}>error</button>
         </div>
     );
 };
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,16 @@
+import { cn } from "shared/lib/classNames/classNames";
+import cls from "./PageError.module.scss";
+import { Button } from "shared/ui/Button/Button";
+
+interface PageErrorProps {
+    className?: string;
+}
+export const PageError = ({ className }: PageErrorProps) => {
+    const reload = () => location.reload();
+    return (
+        <div className={cn(cls.PageError, {}, [className])}>
+            <p>Произошла ошибка, попробуйте обновить страницу</p>
+            <Button onClick={reload}>Обновить</Button>
+        </div>
+    );
+};
