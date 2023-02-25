f8c104c9c27ef3da2d3a68574aa27e202d3b1b63
AirMaks <rm1010@yandex.ru>
2023-02-25 23:11:06 +0300

    add modal , portal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,22 @@
-import "./styles/index.scss";
 import { cn } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar/ui";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Modal } from "shared/ui/Modal/Modal";
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
+    const [isOpen, setIsOpen] = useState(false);
 
+    const closeHandler = () => setIsOpen(false);
     return (
         <div className={cn("app", {}, [theme])}>
             <Suspense fallback="">
                 <Navbar toggleTheme={toggleTheme} />
+                <button onClick={() => setIsOpen(true)}>Toggle</button>
+                <Modal isOpen={isOpen} onClose={closeHandler} />
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,46 @@
+import { cn } from "shared/lib/classNames/classNames";
+import cls from "./Modal.module.scss";
+import { MouseEvent, ReactNode, useCallback, useEffect } from "react";
+import { Portal } from "../Portal/Portal";
+
+interface ModalProps {
+    className?: string;
+    children?: ReactNode;
+    isOpen: boolean;
+    onClose: () => void;
+}
+export const Modal = (props: ModalProps) => {
+    const { className, children, isOpen, onClose } = props;
+    const mods: Record<string, boolean> = {
+        [cls.opened]: isOpen
+    };
+    const closeHandler = useCallback(() => onClose && onClose(), [onClose]);
+    const onKeyDown = useCallback(
+        (e: KeyboardEvent) => {
+            if (e.key === "Escape") {
+                closeHandler();
+            }
+        },
+        [closeHandler]
+    );
+
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener("keydown", onKeyDown);
+        }
+        return () => {
+            window.removeEventListener("keydown", onKeyDown);
+        };
+    }, [isOpen, onKeyDown]);
+    return (
+        <Portal>
+            <div className={cn(cls.Modal, mods, [className])}>
+                <div className={cls.overlay} onClick={closeHandler}>
+                    <div className={cls.content} onClick={(e: MouseEvent) => e.stopPropagation()}>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </Portal>
+    );
+};
