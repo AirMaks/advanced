4bd43cb32894de3c749e10e779e5987a1fbdb5da
AirMaks <rm1010@yandex.ru>
2023-04-08 16:32:20 +0300

    added style lint

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++
@@ -1,4 +0,0 @@
-import { lazy } from "react";
-// delay emulation remove later
-// @ts-ignore
-export const AboutPageAsync = lazy(() => new Promise(resolve => setTimeout(() => resolve(import("./AboutPage")), 1500)));
